91325d35d0e7bae54cbe6acdc3e0bd0c4c108111
Nguyen Minh Nhut <haovanhut74@gmail.com>
2025-07-14 13:43:15 +0700

    connect database (SQL SERVER)

---
+++ Data/DataContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyWebApp.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+}

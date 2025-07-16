6dece12a9185b4af604420c5e80e692d2a5724a0
Nguyen Minh Nhut <haovanhut74@gmail.com>
2025-07-16 10:06:05 +0700

    Thêm chi tiết sản phẩm, fix Index.cshtml trong Home

--- Areas/User/Controllers/ProductController.cs
+++ Areas/User/Controllers/ProductController.cs
@@ -1,17 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyWebApp.Data;
 
 namespace MyWebApp.Areas.User.Controllers;
 
 [Area("User")]
-public class ProductController : Controller
+public class ProductController : BaseController
 {
+    public ProductController(DataContext context) : base(context) { }
+
     public IActionResult Index()
     {
         return View();
     }
 
-    public IActionResult Detail()
+    public async Task<IActionResult> Detail(Guid id)
     {
-        return View();
+        if (id == Guid.Empty)
+        {
+            return RedirectToAction("Index");
+        }
+
+        var product = await _context.Products
+            .Include(p => p.Category)
+            .Include(p => p.Brand)
+            .FirstOrDefaultAsync(p => p.Id == id);
+
+        if (product == null)
+        {
+            // Có thể trả về view "NotFound" hoặc redirect
+            return NotFound(); // hoặc RedirectToAction("Index")
+        }
+
+        return View(product);
     }
+
 }

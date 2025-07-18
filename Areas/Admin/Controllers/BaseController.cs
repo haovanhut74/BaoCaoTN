66ea364313590f43d4614816d079282c13f4f84c
Nguyen Minh Nhut <haovanhut74@gmail.com>
2025-07-18 14:02:29 +0700

    Thêm trang Admin và Quản lý sản phẩm

---
+++ Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebApp.Data;
+
+namespace MyWebApp.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public abstract class BaseController : Controller
+{
+    protected readonly DataContext _context;
+
+    protected BaseController(DataContext context)
+    {
+        _context = context;
+    }
+}
---
+++ Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyWebApp.Data;
+
+namespace MyWebApp.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class ProductController : BaseController
+{
+    public ProductController(DataContext context) : base(context) { }
+
+    public async Task<IActionResult> Index()
+    {
+        var products = await _context.Products.OrderByDescending(p => p.Id).Include(p => p.Category)
+            .Include(p => p.Brand).ToListAsync();
+        return View(products);
+    }
+}

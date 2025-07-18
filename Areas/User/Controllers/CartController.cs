4de17847107e998021094ad3013d5230be6c317e
Nguyen Minh Nhut <haovanhut74@gmail.com>
2025-07-18 09:20:02 +0700

    Thêm giao diện Cart

--- Areas/User/Controllers/CartController.cs
+++ Areas/User/Controllers/CartController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyWebApp.Data;
+using MyWebApp.Extensions;
+using MyWebApp.Models;
+using MyWebApp.ViewModels;
 
 namespace MyWebApp.Areas.User.Controllers;
 
 [Area("User")]
-public class CartController : Controller
+public class CartController : BaseController
 {
+    public CartController(DataContext context) : base(context) { }
+
     public IActionResult Index()
     {
-        return View();
+        // Lấy giỏ hàng từ session, nếu không có thì khởi tạo giỏ hàng mới
+        List<CartItem> cartItems = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+        CartItemViewModel cartViewModel = new CartItemViewModel
+        {
+            CartItems = cartItems,
+            TotalPrice = cartItems.Sum(item => item.Price * item.Quantity)
+        };
+        return View(cartViewModel);
     }
 }

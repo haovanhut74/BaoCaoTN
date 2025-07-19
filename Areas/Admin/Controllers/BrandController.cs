4221825f841673df09b7541ebf1cd4e174e48b08
Nguyen Minh Nhut <haovanhut74@gmail.com>
2025-07-19 19:43:10 +0700

    Thêm giao diện thêm mới Brand

--- Areas/Admin/Controllers/BrandController.cs
+++ Areas/Admin/Controllers/BrandController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyWebApp.Data;
+using MyWebApp.Models;
 
 namespace MyWebApp.Areas.Admin.Controllers;
 
@@ -12,5 +13,53 @@ public class BrandController : BaseController
     {
         var brands = await _context.Brands.OrderByDescending(b => b.Id).ToListAsync();
         return View(brands);
+    }
+    
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Brand brand)
+    {
+        // Kiểm tra Name có null hoặc rỗng không
+        if (string.IsNullOrWhiteSpace(brand.Name))
+        {
+            ModelState.AddModelError(nameof(brand.Name), "Tên danh mục không được để trống");
+            return View(brand);
+        }
+
+        brand.Slug = brand.Name.Replace(" ", "-").ToLower();
+
+        var slug = await _context.Brands.Where(p => p.Slug == brand.Slug).FirstOrDefaultAsync();
+        if (slug != null)
+        {
+            ModelState.AddModelError("", "Danh mục đã có trong Data");
+            return View(brand);
+        }
+
+        if (ModelState.IsValid)
+        {
+            _context.Brands.Add(brand);
+            TempData["message"] = "Thêm mới thành công";
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        TempData["error"] = "Thêm mới thất bại";
+        List<string> errors = [];
+        foreach (var item in ModelState.Values)
+        {
+            foreach (var error in item.Errors)
+            {
+                errors.Add(error.ErrorMessage);
+            }
+        }
+
+        string errorMessage = string.Join("\n", errors);
+        return BadRequest(errorMessage);
     }
 }
